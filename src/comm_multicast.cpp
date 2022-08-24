d76d2659571335815918daf615ff9517088b41a5
Azzam Wildan <wildan.205024@mhs.its.ac.id>
2022-08-24 23:45:31 +0700

    merapikan program

--- src/comm_multicast.cpp
+++ src/comm_multicast.cpp
@@ -21,7 +21,6 @@
 #include <linux/if_ether.h>
 #include "zlib.h"
 #include "lz4.h"
-// #include "iris_routine/utils/shared_data.h"
 #include "ros/ros.h"
 #include "iris_its/config.h"
 #include "iris_its/MulticastTx.h"
@@ -30,9 +29,8 @@
 #include <redis-cpp/execute.h>
 #include <chrono>
 
-// #define COMM_DEBUG // Activate it to debug
-// #define COMM_TEST // Activate it to test the program
-#define COMM_DEBUG_MINOR
+// #define COMM_DEBUG // Debug lebih dalam
+// #define COMM_DEBUG_MINOR // Debug sedikit
 // #define USE_REALTIME_PROCESS // Activate it to use realtime process
 
 #define PERR(txt, par...) \
@@ -72,14 +70,12 @@ char data[128];
 unsigned long int actual_data_size = 15;
 char its[4] = "its";
 
-unsigned long int max_recv_data_size = 128;
+int max_recv_data_size = 128;
 char recv_data[128] = "its1";
 unsigned long int recv_data_size;
 
 struct sockaddr src_addr;
 socklen_t addr_len = sizeof(src_addr);
-
-// SharedData *shared_data;
 
 // ROS
 ros::Timer tim_50hz_send;
@@ -103,7 +99,6 @@ uint16_t status_sub_sub_sub_algoritma;
 
 // KeyDB
 auto keydb_put_stream = rediscpp::make_stream("127.0.0.1", "6969");
-auto keydb_get_stream = rediscpp::make_stream("127.0.0.1", "6969");
 uint8_t toleransi_data_delay = 15;
 int64_t robot_epoch[6];
 
@@ -112,7 +107,6 @@ int8_t data_valid = 0;
 
 void putDBData()
 {
-    // printf("halo\n");
     uint8_t total_item = 4;
     char put_keys[4][7] = {"pos_x", "pos_y", "theta", "r_cond"};
     int8_t put_keys_size[4] = {6, 6, 6, 7};
@@ -132,12 +126,11 @@ void putDBData()
 
     for (int i = 0; i < total_item; i++)
     {
-        memcpy((void *)put_keys[i] + put_keys_size[i] - 1, recv_data + 3, 1);
+        memcpy(put_keys[i] + put_keys_size[i] - 1, recv_data + 3, 1);
         // Final safety
         if (put_keys[i][put_keys_size[i] - 1] >= '1' && put_keys[i][put_keys_size[i] - 1] <= '5') // Robot
         {
             memcpy(&val_buffer_int, recv_data + robot_value_pos[i], robot_value_size[i]);
-            // printf("%s: %d\n", put_keys[i], val_buffer_int);
             auto const put_value = std::to_string(val_buffer_int);
             rediscpp::execute_no_flush(*keydb_put_stream, "set", put_keys[i], put_value);
         }
@@ -158,9 +151,6 @@ void putDBData()
         // printf("%s: %d %d -> %d (%d)\n", s_valid_key, epoch_now, robot_epoch[i], abs(epoch_now - robot_epoch[i]), status_valid);
     }
 
-    // memcpy((void *)s_valid_key + 10, recv_data + 3, 1);
-    // auto const put_value = std::to_string(status_valid);
-    // rediscpp::execute_no_flush(*keydb_put_stream, "set", s_valid_key, put_value);
     std::flush(*keydb_put_stream);
 }
 
@@ -170,7 +160,6 @@ void getDBData()
 
 int if_NameToIndex(char *ifname, char *address)
 {
-    // printf("args: %s %s\n", ifname, address);
     int fd;
     struct ifreq if_info;
     int if_index;
@@ -206,7 +195,7 @@ int if_NameToIndex(char *ifname, char *address)
             (int)((unsigned char *)if_info.ifr_hwaddr.sa_data)[4],
             (int)((unsigned char *)if_info.ifr_hwaddr.sa_data)[5]);
 #ifdef COMM_DEBUG
-    printf("**** Using device %s -> Ethernet %s\n", if_info.ifr_name, address);
+    printf("**** Using device %s -> %s\n", if_info.ifr_name, address);
 #endif
 
     return if_index;
@@ -217,7 +206,7 @@ int openSocket()
     struct ip_mreqn mreqn;
     struct ip_mreq mreq;
     int opt;
-    char address[16]; // IPV4: xxx.xxx.xxx.xxx\0
+    char address[16]; // IPV4: xxx.xxx.xxx.xxx/0
 
     bzero(&multicastAddress, sizeof(struct sockaddr_in));
     multicastAddress.sin_family = AF_INET;
@@ -253,7 +242,6 @@ int openSocket()
     memset((void *)&mreq, 0, sizeof(mreq));
     mreq.imr_multiaddr.s_addr = inet_addr(nw_config.multicast_ip);
     mreq.imr_interface.s_addr = inet_addr(address);
-    // fprintf(stderr, "Index: %d (port %d, %s / %s)\n", multiSocket.socketID, 4321, "224.168.1.80", address);
 
     if ((setsockopt(multiSocket.socketID, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq))) == -1)
     {
@@ -263,7 +251,7 @@ int openSocket()
     }
 
     /* Disable reception of our own multicast */
-    opt = 1; // default 0
+    opt = 0; // default 0
     if ((setsockopt(multiSocket.socketID, IPPROTO_IP, IP_MULTICAST_LOOP, &opt, sizeof(opt))) == -1)
     {
         PERRNO("setsockopt");
@@ -329,7 +317,6 @@ void saveData()
 
     if (msg_multicast_rx.identifier == 0) // Terima dari BS
     {
-        // printf("Hello from BS\n")
         memcpy(&msg_multicast_rx.header, recv_data + 4, 1);
         memcpy(&msg_multicast_rx.command, recv_data + 5, 1);
         memcpy(&msg_multicast_rx.style, recv_data + 6, 1);
@@ -361,8 +348,8 @@ void saveData()
 
     pub_multicast_rx.publish(msg_multicast_rx);
 #ifdef COMM_DEBUG_MINOR
-    // printf("[recv] mux %d\n", msg_multicast_rx.identifier);
-    // printf("[recv] odom: %d %d %d\n", msg_multicast_rx.pos_x_robot, msg_multicast_rx.pos_y_robot, msg_multicast_rx.theta_robot);
+    printf("[recv] mux %d\n", msg_multicast_rx.identifier);
+    // printf("[recv] odom_%d: %d %d %d\n",msg_multicast_rx.identifier, msg_multicast_rx.pos_x_robot, msg_multicast_rx.pos_y_robot, msg_multicast_rx.theta_robot);
     // printf("[recv] bola: %d %d %d\n", msg_multicast_rx.status_bola_robot, msg_multicast_rx.bola_x_pada_lapangan_robot, msg_multicast_rx.bola_y_pada_lapangan_robot);
     // printf("[recv] misc: %d %d %d\n", msg_multicast_rx.status_bola_robot, msg_multicast_rx.bola_x_pada_lapangan_robot, msg_multicast_rx.bola_y_pada_lapangan_robot);
     // printf("[recv] SM: %d %d %d\n", msg_multicast_rx.status_bola_robot, msg_multicast_rx.bola_x_pada_lapangan_robot, msg_multicast_rx.bola_y_pada_lapangan_robot);
@@ -372,7 +359,7 @@ void saveData()
 void loadData()
 {
 #ifdef COMM_DEBUG_MINOR
-    // printf("[send] odom: %d %d %d\n", robot_pos_x, robot_pos_y, robot_pos_theta);
+    printf("[send] odom: %d %d %d\n", robot_pos_x, robot_pos_y, robot_pos_theta);
     // printf("[send] bola: %d %d %d\n", status_bola, bola_x_pada_lapangan, bola_y_pada_lapangan);
 
     // printf("[send] misc: %d %d \n", robot_condition, target_umpan);
@@ -399,36 +386,14 @@ void loadData()
     memcpy(data + 32, &status_sub_sub_sub_algoritma, 2);
 
     actual_data_size = 34; // Selalu update ini ketika merubah data-frame
-    // // printf("data_final: %d %s\n", actual_data_size, data);
-
-    // memcpy(data, its, 3);                      // Header
-    // memcpy(data + 3, nw_config.identifier, 1); // identifier
-    // // Assign data
-    // memcpy(data + 4, &robot_pos_x, 2);
-    // memcpy(data + 6, &robot_pos_y, 2);
-    // memcpy(data + 8, &robot_pos_theta, 2);
-    // memcpy(data + 10, &status_bola, 1);
-    // memcpy(data + 11, &bola_x_pada_lapangan, 2);
-    // memcpy(data + 13, &bola_y_pada_lapangan, 2);
-    // memcpy(data + 15, &robot_condition, 2);
-    // memcpy(data + 17, &target_umpan, 1);
-    // memcpy(data + 18, &status_algoritma, 2);
-    // memcpy(data + 20, &status_sub_algoritma, 2);
-    // memcpy(data + 22, &status_sub_sub_algoritma, 2);
-    // memcpy(data + 24, &status_sub_sub_sub_algoritma, 2);
-    // actual_data_size = 26; // Selalu update ini ketika merubah data-frame
 }
 
 int sendData()
 {
-    // printf("send id %d\n", multiSocket.socketID);
     if (nw_config.compress_type == UNCOMPRESS)
     {
         // Just send
         int nsent = sendto(multiSocket.socketID, data, actual_data_size, 0, (struct sockaddr *)&multiSocket.destAddress, sizeof(struct sockaddr));
-#ifdef COMM_TEST
-        printf("[send] size %d, data %s\n", actual_data_size, data);
-#endif
         if (nsent == actual_data_size)
             return 0;
         else
@@ -440,20 +405,17 @@ int sendData()
         unsigned long int compressed_data_size = compressBound(actual_data_size);
         char compressed_data[compressed_data_size];
 #ifdef COMM_DEBUG
-        printf("Before: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), data);
+        printf("[send] Before: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), data);
 #endif
         compress((Bytef *)compressed_data, &compressed_data_size, (Bytef *)data, (unsigned long)actual_data_size);
 #ifdef COMM_DEBUG
-        printf("After: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), compressed_data);
+        printf("[send] After: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), compressed_data);
         unsigned long int data_raw_size = compressBound(compressed_data_size);
         char data_raw[data_raw_size];
         uncompress((Bytef *)data_raw, &data_raw_size, (Bytef *)compressed_data, (unsigned long)compressed_data_size);
-        printf("Uncompress: %d and %d -> %s\n", sizeof(data_raw), sizeof(compressed_data), data_raw);
+        printf("[send] Uncompress: %d and %d -> %s\n", sizeof(data_raw), sizeof(compressed_data), data_raw);
 #endif
         int nsent = sendto(multiSocket.socketID, compressed_data, compressed_data_size, 0, (struct sockaddr *)&multiSocket.destAddress, sizeof(struct sockaddr));
-#ifdef COMM_TEST
-        printf("[send] size %d, data %s\n", compressed_data_size, compressed_data);
-#endif
         if (nsent == compressed_data_size)
             return 0;
         else
@@ -465,20 +427,17 @@ int sendData()
         int compressed_data_size = LZ4_compressBound(actual_data_size);
         char compressed_data[compressed_data_size];
 #ifdef COMM_DEBUG
-        printf("Before: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), data);
+        printf("[send] Before: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), data);
 #endif
         compressed_data_size = LZ4_compress_default(data, &compressed_data[0], actual_data_size, compressed_data_size);
 #ifdef COMM_DEBUG
-        printf("After: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), compressed_data);
+        printf("[send] After: %d and %d -> %s\n", sizeof(data), sizeof(compressed_data), compressed_data);
         unsigned long int data_raw_size = LZ4_compressBound(compressed_data_size);
         char data_raw[data_raw_size];
         data_raw_size = LZ4_decompress_safe(compressed_data, &data_raw[0], compressed_data_size, data_raw_size);
-        printf("Uncompress: %d and %d -> %s\n", sizeof(data_raw), sizeof(compressed_data), data_raw);
+        printf("[send] Uncompress: %d and %d -> %s\n", sizeof(data_raw), sizeof(compressed_data), data_raw);
 #endif
         int nsent = sendto(multiSocket.socketID, compressed_data, compressed_data_size, 0, (struct sockaddr *)&multiSocket.destAddress, sizeof(struct sockaddr));
-#ifdef COMM_TEST
-        printf("[send] size %d, data %s\n", compressed_data_size, compressed_data);
-#endif
         if (nsent == compressed_data_size)
             return 0;
         else
@@ -490,33 +449,19 @@ int sendData()
 
 int recvData()
 {
-    // struct sockaddr src_addr;
-    // socklen_t addr_len;
     char recv_buffer[max_recv_data_size] = "its";
     // int nrecv = recvfrom(recv_socket->socketID, (void *)recv_buffer, max_recv_data_size, MSG_DONTWAIT, &src_addr, &addr_len);
     int nrecv = recvfrom(recv_socket->socketID, recv_buffer, max_recv_data_size, 0, &src_addr, &addr_len);
-    // printf("Buffer %d, nrecv: %d | %d -> %s\n", recv_socket->socketID, nrecv, errno, recv_buffer);
     if (nrecv == -1 && errno == 22 && recv_buffer[0] == 'i')
     {
         printf("Error occured, restart socket %s\n", recv_buffer);
-        // closeSocket();
-        // if (openSocket() == -1)
-        // {
-        //     PERR("openMulticastSocket");
-        //     // return -1;
-        // }
         return -1;
     }
-#ifdef COMM_DEBUG
-    printf("Buffer, nrecv: %d -> %s\n", nrecv, recv_buffer);
-#endif
     if (nrecv > 0 && recv_buffer[0] == 'i')
     {
-        // printf("data: %s\n", recv_buffer);
         bzero(recv_data, max_recv_data_size);
         if (nw_config.compress_type == UNCOMPRESS)
         {
-            // strncpy(recv_data, recv_buffer, nrecv);
             recv_data_size = nrecv;
             memcpy(recv_data, recv_buffer, nrecv);
         }
@@ -527,15 +472,9 @@ int recvData()
         }
         else if (nw_config.compress_type == LZ4_COMPRESS)
         {
-            // recv_data_size = LZ4_compressBound(nrecv);
-            recv_data_size = compressBound(nrecv);
-            // printf("size: %d and %d\n", recv_data_size, nrecv);
+            recv_data_size = LZ4_compressBound(nrecv);
             recv_data_size = LZ4_decompress_safe(recv_buffer, &recv_data[0], nrecv, recv_data_size);
         }
-        // char *src_ip = inet_ntoa(((struct sockaddr_in *)&src_addr)->sin_addr);
-#if defined(COMM_DEBUG) || defined(COMM_TEST)
-        // printf("[recv] recv_from: %s\n", src_ip);
-#endif
         if (recv_data_size > 0)
             saveData();
     }
@@ -545,28 +484,6 @@ int recvData()
 
 void callbackSubDataTx(const iris_its::MulticastTxPtr &msg)
 {
-    // #ifdef COMM_DEBUG_MINOR
-    //     printf("[send] odom: %d %d %d\n", msg->pos_x, msg->pos_y, msg->theta);
-    //     printf("[send] bola: %d %d %d\n", msg->status_bola, msg->bola_x_pada_lapangan, msg->bola_y_pada_lapangan);
-    // #endif
-
-    //     memcpy(data, its, 3);                      // Header
-    //     memcpy(data + 3, nw_config.identifier, 1); // identifier
-    //     memcpy(data + 4, &msg->pos_x, 2);
-    //     memcpy(data + 6, &msg->pos_y, 2);
-    //     memcpy(data + 8, &msg->theta, 2);
-    //     memcpy(data + 10, &msg->status_bola, 1);
-    //     memcpy(data + 11, &msg->bola_x_pada_lapangan, 2);
-    //     memcpy(data + 13, &msg->bola_y_pada_lapangan, 2);
-    //     memcpy(data + 15, &msg->robot_condition, 2);
-    //     memcpy(data + 17, &msg->target_umpan, 1);
-    //     memcpy(data + 18, &msg->status_algoritma, 2);
-    //     memcpy(data + 20, &msg->status_sub_algoritma, 2);
-    //     memcpy(data + 22, &msg->status_sub_sub_algoritma, 2);
-    //     memcpy(data + 24, &msg->status_sub_sub_sub_algoritma, 2);
-
-    //     actual_data_size = 26;
-
     robot_pos_x = msg->pos_x;
     robot_pos_y = msg->pos_y;
     robot_pos_theta = msg->theta;
@@ -598,7 +515,6 @@ void callbackSubDataTx(const iris_its::MulticastTxPtr &msg)
 
 void cllbckTim50HzSend(const ros::TimerEvent &event)
 {
-    // printf("[send] hello \n");
     if (data_valid)
     {
         loadData();
@@ -611,20 +527,17 @@ void cllbckTim50HzSend(const ros::TimerEvent &event)
 }
 void cllbckTim50HzRecv(const ros::TimerEvent &event)
 {
-    // printf("[recv] hello \n");
     if (recvData() == -1)
     {
         PERR("recvData");
         closeSocket();
         ros::shutdown();
     }
-    // saveData();
 }
 
 void resetStatusData()
 {
     memset(robot_epoch, 0, 6);
-
     char s_valid_key1[16] = "data_valid1";
     char s_valid_key2[16] = "data_valid2";
     char s_valid_key3[16] = "data_valid3";
@@ -648,33 +561,13 @@ void signalHandler(int sig)
 
 int main(int argc, char **argv)
 {
-    // sleep(1.5);
-
     ros::init(argc, argv, "comm_multicast");
     ros::NodeHandle NH;
     ros::MultiThreadedSpinner MTS;
 
-    // keydb_put_stream = rediscpp::make_stream("127.0.0.1", "6969");
-
-    // auto keydb_put_stream = rediscpp::make_stream("127.0.0.1", "6969");
-    // auto keydb_get_stream = rediscpp::make_stream("127.0.0.1", "6969");
-
-    // const char put_keys[3][7] = {"pos_x1", "pos_y1", "theta1"};
-    // const char get_keys[3][7] = {"pos_x", "pos_y", "theta"};
-
-    // Put
-
-    // return 0;
-
     loadConfig();
-
     signal(SIGINT, signalHandler);
-
     resetStatusData();
-
-    // shared_data = SharedData::getInstance();
-    // printf("awal %d\n", shared_data->status_bola);
-    //
 
 #ifdef USE_REALTIME_PROCESS
     struct sched_param proc_sched;
@@ -693,12 +586,8 @@ int main(int argc, char **argv)
     }
 
     recv_socket = &multiSocket;
-
-    // printf("send socket id: %d\n", multiSocket.socketID); //s
-
     sub_multicast_tx = NH.subscribe("multicast_tx", 16, &callbackSubDataTx);
     pub_multicast_rx = NH.advertise<iris_its::MulticastRx>("multicast_rx", 16);
-
     tim_50hz_send = NH.createTimer(ros::Duration(0.1), cllbckTim50HzSend);
     tim_50hz_receive = NH.createTimer(ros::Duration(0.01), cllbckTim50HzRecv);
 
